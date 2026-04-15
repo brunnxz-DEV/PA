396c0f8cf445f42ea061aa309ee16d005f250f7b
brunnxz <bruninhosoares441@gmail.com>
2026-04-15 12:32:24 -0300

    Add files via upload

---
+++ Exercicio 2/src/exercicio/pkg2/Exercicio2.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exercicio.pkg2;
+
+/**
+ *
+ * @author Admin
+ */
+public class Exercicio2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // declaração de variaveis sem valor
+        int idade;
+        double valor;
+        char sexo;
+        // atribuição dos valores das variaveis
+        idade = 15;
+        valor = 39.99;
+        sexo = 'M';
+        System.out.println("idede e " + idade);
+        System.out.println("O vcalor e " + valor);
+    }
+    
+}
