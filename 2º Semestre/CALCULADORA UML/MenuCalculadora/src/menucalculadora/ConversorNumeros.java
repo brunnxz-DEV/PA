ae50622a45bc1aa100f92bfb896bf7b0f3a06be9
brunnxz <bruninhoamorim441@gmail.com>
2026-09-23 09:18:33 -0300

    Add files via upload

---
+++ 2º Semestre/CALCULADORA UML/MenuCalculadora/src/menucalculadora/ConversorNumeros.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package menucalculadora;
+
+public class ConversorNumeros {
+    public Integer StringToInt(String num){
+        int conversor = Integer.parseInt(num);
+        return(conversor);
+    }
+    public Double stringToDouble(String num){
+        double conversor = Double.parseDouble(num);
+        return (conversor);
+    }
+}
---
+++ 2º Semestre/CALCULADORA UML/MenuCalculadora/src/menucalculadora/EntradaSaidaDados.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package menucalculadora;
+import javax.swing.JOptionPane;
+
+public class EntradaSaidaDados {
+    public String entradaDados(String mensagemEntrada){
+        return JOptionPane.showInputDialog(mensagemEntrada);
+    }
+    public void saidaDados(String mensagemSaida){
+        JOptionPane.showMessageDialog(null, mensagemSaida);
+    }
+}
